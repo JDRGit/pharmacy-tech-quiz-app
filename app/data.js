7a1e2e0b73182c82726ae6e4dbe3a7e1d6142c4f
JDRGit <jaimerodriguez85@gmail.com>
2023-05-30 13:07:46 -0400

    initial commit, working state

---
+++ app/data.js
@@ -0,0 +1,158 @@
+export const quiz = {
+  totalQuestions: 20,
+  questions: [
+    {
+      id: 1,
+      question: "What is the generic name for the drug Lipitor?",
+      answers: ["Atorvastatin", "Simvastatin", "Rosuvastatin", "Lovastatin"],
+      correctAnswer: "Atorvastatin",
+    },
+    {
+      id: 2,
+      question: "Which drug classification is used to treat hypertension?",
+      answers: ["Statins", "Antifungals", "Antivirals", "ACE inhibitors"],
+      correctAnswer: "ACE inhibitors",
+    },
+    {
+      id: 3,
+      question: 'What does the abbreviation "qhs" stand for?',
+      answers: ["Every hour", "Every day", "At bedtime", "As needed"],
+      correctAnswer: "At bedtime",
+    },
+    {
+      id: 4,
+      question:
+        "Which of the following medications is a proton pump inhibitor?",
+      answers: ["Omeprazole", "Metformin", "Loratadine", "Acetaminophen"],
+      correctAnswer: "Omeprazole",
+    },
+    {
+      id: 5,
+      question: "What is the primary purpose of a pharmacy technician?",
+      answers: [
+        "To prescribe medications",
+        "To diagnose illnesses",
+        "To assist the pharmacist in dispensing medication",
+        "To perform surgeries",
+      ],
+      correctAnswer: "To assist the pharmacist in dispensing medication",
+    },
+    {
+      id: 6,
+      question: "Which category of drugs is used to relieve pain?",
+      answers: ["Analgesics", "Diuretics", "Antidepressants", "Antihistamines"],
+      correctAnswer: "Analgesics",
+    },
+    {
+      id: 7,
+      question: "What is the brand name for the drug alprazolam?",
+      answers: ["Xanax", "Valium", "Prozac", "Zoloft"],
+      correctAnswer: "Xanax",
+    },
+    {
+      id: 8,
+      question:
+        "Which of the following is a common side effect of corticosteroids?",
+      answers: [
+        "Increased appetite",
+        "Decreased heart rate",
+        "Insomnia",
+        "All of the above",
+      ],
+      correctAnswer: "All of the above",
+    },
+    {
+      id: 9,
+      question: 'What does the term "contraindication" mean?',
+      answers: [
+        "A reason to take a medication",
+        "A reason to avoid a medication",
+        "A side effect of a medication",
+        "None of the above",
+      ],
+      correctAnswer: "A reason to avoid a medication",
+    },
+    {
+      id: 10,
+      question: 'What is the common use for the drug class "beta blockers"?',
+      answers: [
+        "Lowering blood pressure",
+        "Treating depression",
+        "Relieving pain",
+        "Treating infections",
+      ],
+      correctAnswer: "Lowering blood pressure",
+    },
+    {
+      id: 11,
+      question: "Which medication is used to treat Type 2 diabetes?",
+      answers: ["Metformin", "Warfarin", "Lisinopril", "Amoxicillin"],
+      correctAnswer: "Metformin",
+    },
+    {
+      id: 12,
+      question: "Which of the following is an opioid medication?",
+      answers: ["Morphine", "Losartan", "Levothyroxine", "Lisinopril"],
+      correctAnswer: "Morphine",
+    },
+    {
+      id: 13,
+      question: 'What does "PO" stand for in medical terminology?',
+      answers: ["By mouth", "By injection", "By rectum", "By inhalation"],
+      correctAnswer: "By mouth",
+    },
+    {
+      id: 14,
+      question: "What is the role of an antibiotic?",
+      answers: [
+        "Relieve pain",
+        "Reduce inflammation",
+        "Fight bacterial infections",
+        "Lower cholesterol",
+      ],
+      correctAnswer: "Fight bacterial infections",
+    },
+    {
+      id: 15,
+      question: "Which medication is used to treat allergic reactions?",
+      answers: ["Epinephrine", "Lorazepam", "Metoprolol", "Simvastatin"],
+      correctAnswer: "Epinephrine",
+    },
+    {
+      id: 16,
+      question: "Which of the following medications is an anticoagulant?",
+      answers: ["Warfarin", "Metformin", "Atorvastatin", "Furosemide"],
+      correctAnswer: "Warfarin",
+    },
+    {
+      id: 17,
+      question: 'What does "PRN" stand for in medical terminology?',
+      answers: ["Every day", "As needed", "Before meals", "After meals"],
+      correctAnswer: "As needed",
+    },
+    {
+      id: 18,
+      question: "Which medication is used to treat high cholesterol?",
+      answers: ["Atorvastatin", "Metformin", "Lisinopril", "Amoxicillin"],
+      correctAnswer: "Atorvastatin",
+    },
+    {
+      id: 19,
+      question: 'What does the term "bioavailability" mean?',
+      answers: [
+        "The amount of drug that is excreted",
+        "The amount of drug that is absorbed and available for use",
+        "The amount of drug that is metabolized",
+        "The amount of drug that is prescribed",
+      ],
+      correctAnswer:
+        "The amount of drug that is absorbed and available for use",
+    },
+    {
+      id: 20,
+      question: "Which medication is typically used to treat epilepsy?",
+      answers: ["Phenytoin", "Losartan", "Metformin", "Atorvastatin"],
+      correctAnswer: "Phenytoin",
+    },
+  ],
+};
